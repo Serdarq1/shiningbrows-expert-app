79d4aa829679dacf980d78e22050ee8b74606015
Serdar Ziya Akova <serdar.akova5@gmail.com>
2026-04-11 12:12:21 +0300

    Change service worker file

--- static/js/service-worker.js
+++ static/js/service-worker.js
@@ -1,12 +1,1 @@
-self.addEventListener("install", (event) => {
-  self.skipWaiting();
-});
-
-self.addEventListener("activate", (event) => {
-  event.waitUntil(self.clients.claim());
-});
-
-// Simple network-first fetch to allow offline fallback later if needed
-self.addEventListener("fetch", () => {
-  // no-op: keeping default network behavior, but SW is registered for PWA install
-});
+importScripts("https://progressier.app/V4lkkozJ38gPODhAh2lu/sw.js");
